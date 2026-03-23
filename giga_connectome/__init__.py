0fb61e7c60ad7ab4cfb01dddeed0d9baefbf6da5
Remi Gau <remi_gau@hotmail.com>
2026-03-23 18:01:42 +0100

    fix

--- giga_connectome/__init__.py
+++ giga_connectome/__init__.py
@@ -1,14 +1,9 @@
 import contextlib
 
-__packagename__ = "giga_connectome"
-__copyright__ = "2025, BIDS-Apps"
-
 with contextlib.suppress(ImportError):
     from ._version import __version__
 
 
 __all__ = [
-    "__copyright__",
-    "__packagename__",
     "__version__",
 ]
